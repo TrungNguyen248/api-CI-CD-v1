9e84e9c03c52d5b7032a86b99cd1910cfaedf21c
nguyennt <nguyentrungnguyen12a3@gmail.com>
2024-04-20 22:53:06 +0700

    FEATURE: add router get users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
-
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+    metadata: [
+      {
+        name: "test1",
+        age: "11",
+        address: "Nam liber",
+      },
+      {
+        name: "test2",
+        age: "10",
+        address: "Nam strong liber",
+      },
+      {
+        name: "test3",
+        age: "13",
+        address: "Nam die liber",
+      },
+      {
+        name: "test4",
+        age: "17",
+        address: "Nam back liber",
+      },
+      {
+        name: "test5",
+        age: "100",
+        address: "Nam die liber",
+      },
+    ],
+  });
+});
 module.exports = router;
